e16cab6fcb0f04443f5983595b75e24b083b0e63
Jair Alexander Luna <79240739+Jair-Luna@users.noreply.github.com>
2022-09-01 15:07:57 -0500

    Revert "Ayudas"

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('mainBtn').addEventListener('click', () => {
-	decir(document.getElementById('mainTxt').innerHTML);
-});
-
-function decir(texto) {
-	speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
-}
